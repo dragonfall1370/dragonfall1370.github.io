a331dfae39d85186d8f604620a6d379001e7d3f9
Long Vu <75563144+dragonfall1370@users.noreply.github.com>
2022-08-30 16:25:44 +0200

    Delete run/eu_redshift_models/models directory

--- run/eu_redshift_models/models/core/fct/shopify_global/cohort_and_retention_processing/new_cohort_and_retention_processing/product_point_of_view/archive/fct_shopify_product_indirect_retention_all_orders.sql
+++
@@ -1,5 +0,0 @@
-
-
-  create view "airup_eu_dwh"."zzz_long_test"."fct_shopify_product_indirect_retention_all_orders__dbt_tmp" as (
-    select 1 from "airup_eu_dwh"."dbt_feldm"."fct_shopify_cohort_processing_new"
-  ) with no schema binding;
--- run/eu_redshift_models/models/core/fct/shopify_global/cohort_and_retention_processing/new_cohort_and_retention_processing/product_point_of_view/fct_shopify_product_retention_first_order.sql
+++
@@ -1,9 +0,0 @@
-
-
-  create view "airup_eu_dwh"."dbt_feldm"."fct_shopify_product_retention_first_order__dbt_tmp" as (
-    
-
-select * from  "airup_eu_dwh"."dbt_feldm"."fct_shopify_product_retention_first_order_products"
-union all
-select * from  "airup_eu_dwh"."dbt_feldm"."fct_shopify_product_retention_first_order_drinking_systems"
-  ) with no schema binding;
